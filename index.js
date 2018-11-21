44bd3620bd702242924512ccc795f14f61ceb255
Vengerov <hardbeat34@gmail.com>
2018-11-21 23:52:42 +0300

    some

---
+++ index.js
@@ -0,0 +1,12 @@
+const two_sum = require('./lib/two_sum')
+const add_two_numbers = require('./lib/add_two_numbers')
+
+
+nums   = [2,11,7,3,5,15]
+target = 9
+result = two_sum(nums, target)
+
+console.log(result)
+
+
+
